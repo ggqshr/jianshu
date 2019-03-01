2295de39eb69f63edf9eff3b7bfb615e83bcd40f
unknown <ggq18663278150@sina.com>
2019-03-01 13:48:07 +0800

    写了简书的header添加了iconfont

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Globalstyle } from './style'
+// import { Globalstyle } from './style'
+import Header from './common/header'
+
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Globalstyle />
-        hello world
-      </div>
+      <Header/>
     );
   }
 }
