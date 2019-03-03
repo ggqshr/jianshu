90b69964226c698ee4a63752dc4887363b0fe9b1
ggqshr <942490944@qq.com>
2019-03-03 08:24:25 +0800

    开始修改list

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ListInfo, ListItem } from '../style'
+import { ListInfo, ListItem,LoadMore } from '../style'
 import { connect } from 'react-redux'
 
 class List extends Component {
@@ -18,13 +18,9 @@ class List extends Component {
             </ListItem>
           )
         })}
-        <ListItem>
-          <img className='pic' src='https://upload-images.jianshu.io/upload_images/15400793-08a6c1afa56b1903?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240' />
-          <ListInfo>
-            <h3 className='title'>互联网运营必备的八大数据分析模型！</h3>
-            <p className='desc'>今天来聊聊互联网运营中常用到的数据分析模型： 1、用户模型 “不仅要知道用户当下在想什么，更要知道用户背后在想什么，以及用户正在经历着什么。” ...</p>
-          </ListInfo>
-        </ListItem>
+        <LoadMore>
+          更多文字
+        </LoadMore>
       </div>
     )
   }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -93,3 +93,14 @@ export const TopicItem = styled.div`
     }
 `;
 
+
+export const LoadMore = styled.div`
+    width:100%;
+    height:40px;
+    line-height:40px;
+    text-align:center;
+    background:#a5a5a5;
+    border-radius:20px;
+    color:#fff;
+    margin:30px 0;
+`;
